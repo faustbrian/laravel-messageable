e0845c7f536d867a2ed8cebcd1a8b12cc59b5ed1
Brian Faust <brian@faust.de.com>
2024-11-05 07:38:57 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ src/Concerns/HasMessages.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Messageable\Concerns;
+
+use BaseCodeOy\Messageable\Models\Message;
+use BaseCodeOy\Messageable\Models\Participant;
+use BaseCodeOy\Messageable\Models\Thread;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+trait HasMessages
+{
+    public function messages(): MorphMany
+    {
+        return $this->morphMany(Message::class, 'creator');
+    }
+
+    public function threads(): BelongsToMany
+    {
+        return $this->belongsToMany(Thread::class, 'participants', 'participant_id');
+    }
+
+    public function newMessagesCount(): int
+    {
+        return \count($this->threadsWithNewMessages());
+    }
+
+    public function threadsWithNewMessages(): array
+    {
+        $threadsWithNewMessages = [];
+        $participants = Participant::where('participant_id', $this->id)
+            ->where('participant_type', \get_class($this))
+            ->lists('last_read', 'thread_id');
+
+        if ($participants) {
+            $threads = Thread::whereIn('id', \array_keys($participants->toArray()))->get();
+
+            foreach ($threads as $thread) {
+                if ($thread->updated_at > $participants[$thread->id]) {
+                    $threadsWithNewMessages[] = $thread->id;
+                }
+            }
+        }
+
+        return $threadsWithNewMessages;
+    }
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Messageable;
+
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    //
+}
